4c69c9b8680705e7a6424f28c87bf63671c354a7
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-18 21:16:57 +0530

    Complete the sign-up view

--- app.js
+++ app.js
@@ -14,10 +14,25 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
 
 const app = express();
+// Require static assets from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set 'views' directory for any views 
+// being rendered res.render()
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
 });
+
+app.get('/sign-up',(req,res)=>{
+    res.render('sign-up');
+})
 
 
 
